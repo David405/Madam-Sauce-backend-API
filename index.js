de4d3ebbf2082baa66029e799aa99cccd23c2e05
David Asamonye <davidasamonye@Davidc.local>
2020-10-14 22:44:01 +0100

    added a template engine

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const path = require('path');
+const { config, engine } = require('express-edge');
 const express = require('express');
 
 const app = new express();
 
 app.use(express.static('public'));
+app.use(engine);
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/createorder', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/order.html'));
+})
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
